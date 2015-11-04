772ed532f40d87c56bd3314d2f7c1f6a617400ba
William Izzo <william.izzo.mail@gmail.com>
2015-11-04 13:50:45 +0100

    added gps io service

---
+++ Places/io/Gps.swift
@@ -0,0 +1,22 @@
+//
+//  Gps.swift
+//  Places
+//
+//  Created by William Izzo on 04/11/15.
+//  Copyright © 2015 wizzo s.l.d.s. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+
+private class GpsSession : NSObject, CLLocationManagerDelegate {
+    private lazy var locationManager : CLLocationManager!
+    
+}
+
+class Gps {
+    static func beginSession(authStatus : CLAuthorizationStatus) {
+        
+    }
+}
